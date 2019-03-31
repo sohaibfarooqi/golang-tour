70973a711e8d51fe0b03093da8fff4deff83a627
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-03-31 12:33:40 +0800

    switch statements

---
+++ switch.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "fmt"
+  "runtime"
+)
+
+func main(){
+  /*
+  Go switch statments run only the selected case and not all the cases
+  that follow i.e. Go inserts break statement itself. Cases are evaluated
+  from top to bottom.
+  */
+  switch os := runtime.GOOS; os {
+
+  case "darwin":
+    fmt.Println("OS X")
+
+  case "linux":
+    fmt.Println("Linux.")
+
+  default:
+    fmt.Println(os)
+
+  }
+}
