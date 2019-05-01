572b46a85215b7f3740383a15a6f1d19ef0661e5
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-05-01 23:35:42 +0800

    closures in go

---
+++ closures.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/*
+Functions are values too in Golang. They can be
+pass as an argument to other functions.
+*/
+func adder() func(int) int{
+  sum := 0
+  return func(x int) int{
+    sum += x
+    return sum
+  }
+}
+
+func main(){
+  pos, neg := adder(), adder()
+  for i:=0; i<10; i++{
+    fmt.Println(pos(i), neg(-2*i))
+  }
+}
