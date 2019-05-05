e0b43a29c80899d42cefb28479e905e08ddb28a9
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-05-05 23:33:47 +0800

    updated with pointer receiver

--- methods.go
+++ methods.go
@@ -19,7 +19,36 @@ func(v Vertex) Abs() float64{
   return math.Sqrt(v.X * v.X + v.Y * v.Y)
 }
 
+/*
+Methods can be declared on non-struct types too.
+*/
+type MyFloat float64
+
+func (f MyFloat) Abs() float64{
+  if f < 0 {
+    return float64(-f)
+  }
+  return float64(f)
+}
+
+/*
+Receiver argument can be a pointer too.
+This will modify the original argumnet passed
+instead of modifying a copy.
+*/
+func (v *Vertex) Scale(f float64){
+  v.X = v.X * f
+  v.Y = v.Y * f
+}
 func main(){
   v := Vertex{2,3}
   fmt.Println(v.Abs())
+
+  f := MyFloat(-math.Sqrt2)
+  fmt.Println(f.Abs())
+
+  // For convenience `v.Scale(..)` is acceptable instead of (&v).Scale(..)
+  v.Scale(10)
+  fmt.Println(v.Abs())
+
 }
